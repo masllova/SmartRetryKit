428ef63b77fbad8b18a593a90f89a9bc5b0e7cfa
Alexandra <allexa.msv@gmail.com>
2026-03-20 21:51:45 +0300

    fix

--- Sources/SmartRetryKit/Retry.swift
+++ Sources/SmartRetryKit/Retry.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 public struct Retry {
-    public static var isLoggingEnabled = true
-    
     public static func run<T>(
         delays: [TimeInterval],
         operation: @escaping () async throws -> T,
@@ -12,31 +10,31 @@ public struct Retry {
         
         for (index, delay) in delays.enumerated() {
             do {
-                log("🚀 Attempt \(index + 1)")
+                print("🚀 Attempt \(index + 1)")
                 let result = try await operation()
                 
                 if let validate = validate {
                     if validate(result) {
-                        log("✅ Success on attempt \(index + 1)")
+                        print("✅ Success on attempt \(index + 1)")
                         return result
                     } else {
-                        log("⚠️ Invalid result (validation failed)")
+                        print("⚠️ Invalid result (validation failed)")
                         lastError = RetryError.maxRetriesReached
                     }
                 } else {
-                    log("✅ Success on attempt \(index + 1)")
+                    print("✅ Success on attempt \(index + 1)")
                     return result
                 }
             } catch {
-                log("❌ Error on attempt \(index + 1): \(error)")
+                print("❌ Error on attempt \(index + 1): \(error)")
                 lastError = error
             }
             if index < delays.count - 1 {
-                log("🔁 Retry in \(format(delay))")
+                print("🔁 Retry in \(format(delay))")
                 try await sleep(seconds: delay)
             }
         }
-        log("🛑 Max retries reached")
+        print("🛑 Max retries reached")
         throw lastError ?? RetryError.maxRetriesReached
     }
 }
